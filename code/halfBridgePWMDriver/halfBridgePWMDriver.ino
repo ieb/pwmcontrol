031a694bb7efe25a0f9111357efd3e2d07374b30
Ian Boston <ieb@tfd.co.uk>
2019-02-24 17:10:55 +0000

    Fixed code, both high and hbridge now working

--- code/halfBridgePWMDriver/halfBridgePWMDriver.ino
+++ code/halfBridgePWMDriver/halfBridgePWMDriver.ino
@@ -8,17 +8,20 @@
 
 #define MASTER_CLOCK 84000000
 uint32_t clock_a = 42000000; // Sampling frequency in Hz
-unit32_t steps = 600;
+uint32_t steps = 600;
+uint32_t frequency = 42000000/(2*steps);
+uint32_t clockFrequency = 42000000;
+uint32_t dutyCycle = 100;
 
-#define HBRIDGE 1
+//#define HBRIDGE 1
 
 
 // if the base cycles is 2 the drvier is configured as a H bridge, if 1 its configured as a 
 // high or low side driver only.
 #ifdef HBRIDGE
-#define BASE_CYCLES 2;
+#define BASE_CYCLES 2
 #else
-#define BASE_CYCLES 1;
+#define BASE_CYCLES 1
 #endif
 
 void setup() 
@@ -45,18 +48,15 @@ void setup()
   PWMC_SetDutyCycle(PWM, 0, steps/BASE_CYCLES); // Channel: 0, Duty cycle: 50 %
   PWMC_SetDeadTime(PWM, 0, 42, 42); // Channel: 0, Rising and falling edge dead time: 42/42 Mhz = 1 us
 #else
-  PWMC_ConfigureChannelExt(PWM,
+  PWMC_ConfigureChannel(PWM,
                            0, // Channel: 0          
                            PWM_CMR_CPRE_CLKA, // Prescaler: use CLOCK_A
                            0, // Alignment: period start aligned
-                           0, // Polarity: output waveform starts at a low level
-                           0, // Counter event: none
-                           PWM_CMR_DTE, // Dead time generator is disabled
-                           0, // Dead time PWMH output is not inverted    
-                           0);  // Dead time PWML output is not inverted
+                           0 // Polarity: output waveform starts at a low level
+                           );
   PWMC_SetPeriod(PWM, 0, steps); // Channel: 0, Period: 1/(1200/42 Mhz) = ~35 kHz
   PWMC_SetDutyCycle(PWM, 0, steps); // Channel: 0, Duty cycle: 100 %
-  PWMC_SetDeadTime(PWM, 0, 42, 42); // Channel: 0, Rising and falling edge dead time: 42/42 Mhz = 1 us
+//  PWMC_SetDeadTime(PWM, 0, 42, 42); // Channel: 0, Rising and falling edge dead time: 42/42 Mhz = 1 us
 #endif
   PWMC_EnableChannel(PWM, 0); // Channel: 0
   Serial.begin(115200);
@@ -96,17 +96,31 @@ void setFrequency(uint32_t newFrequency) {
   Serial.print((1.0*clockFrequency)/(steps*BASE_CYCLES));
   Serial.println(" Hz Pwm ");
 }
-void setSteps(unit32_t newSteps) {
+void setSteps(uint32_t newSteps) {
+#ifdef HBRIDGE
   steps = newSteps;
   setFrequency(frequency);
   PWMC_SetPeriod(PWM, 0, steps); // Channel: 0, Period: 1/(1200/42 Mhz) = ~35 kHz
   PWMC_SetDutyCycle(PWM, 0, steps/BASE_CYCLES); // Channel: 0, Duty cycle: 50 %
   Serial.print(steps);
   Serial.print(" Steps ");
-  Serial.print((1.0*clock_a)/(steps*BASE_CYCLES)
-  Serial.print(" Hz max frequency ");
+  Serial.print((1.0*clock_a)/(steps*BASE_CYCLES));
+  Serial.println(" Hz max frequency ");
+#else
+  steps = newSteps;
+  setFrequency(frequency);
+  PWMC_SetPeriod(PWM, 0, steps); // Channel: 0, Period: 1/(1200/42 Mhz) = ~35 kHz
+  setDutyCycle(dutyCycle);
+  Serial.print(steps);
+  Serial.print(" Steps ");
+  Serial.print((1.0*clock_a)/(steps*BASE_CYCLES));
+  Serial.println(" Hz max frequency ");
+#endif
+
 }
-void setDutyCycle(uint32_t dutyCycle) {
+void setDutyCycle(uint32_t newDutyCycle) {
+#ifdef HBRIDGE
+  dutyCycle = newDutyCycle;
     // make the dead time 1us (42/42MHz) minimum.
     int deadTime = 42+(((steps-42)*(100-dutyCycle))/100);
     PWMC_SetDeadTime(PWM, 0, deadTime, deadTime); // Channel: 0, Rising and falling edge dead time: 42/42 Mhz = 1 us
@@ -114,6 +128,16 @@ void setDutyCycle(uint32_t dutyCycle) {
     Serial.print("% Dutycycle gives ");
     Serial.print((1.0*deadTime)/(clockFrequency/1000000.0));
     Serial.println("us Deadtime");
+#else
+  dutyCycle = newDutyCycle;
+  int dutyCycleSteps = (steps*dutyCycle)/100;
+  PWMC_SetDutyCycle(PWM, 0, dutyCycleSteps); // Channel: 0
+  Serial.print(dutyCycle);
+  Serial.print("% Dutycycle gives ");
+  Serial.print(dutyCycleSteps);
+  Serial.println(" steps");
+#endif
+
 }
 
 void SetPin(uint8_t pin)
